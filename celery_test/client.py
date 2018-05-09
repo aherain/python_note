e45d06966297e2c8b3726b16a1e89e420bf23979
aherain <1364091689@qq.com>
2018-05-09 10:47:28 +0800

    'updateclient.py'

--- celery_test/client.py
+++ celery_test/client.py
@@ -1,9 +1,13 @@
 # -*- coding: utf-8 -*-
-
+import time
 from celery_app import task1
 from celery_app import task2
 
-task1.add.apply_async(args=[2, 8])        # 也可用 task1.add.delay(2, 8)
-task2.multiply.apply_async(args=[3, 7])   # 也可用 task2.multiply.delay(3, 7)
 
-print 'hello world'
+if True:
+    task1.add.apply_async(args=[2, 8])  # 也可用 task1.add.delay(2, 8)
+    task2.multiply.apply_async(args=[3, 7])  # 也可用 task2.multiply.delay(3, 7)
+    print '计算任务已经派发，5秒后将进行下一次任务派发'
+    time.sleep(5)
+
+
