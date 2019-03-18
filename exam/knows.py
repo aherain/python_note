3112f099b09dab3f65df1fcee214640ed180379d
heyu <heyu@huayingjuhe.com>
2019-03-18 15:35:36 +0800

    'update'

---
+++ exam/knows.py
@@ -0,0 +1,32 @@
+from io import StringIO
+
+
+f = StringIO()
+a = f.write('nihao')
+print(a)
+b = f.write(' 2019')
+print(b)
+
+print(f.getvalue())
+
+
+t = StringIO('Hello!\nHi!\nGoodbye')
+while True:
+    s = t.readline()
+    if s == '':
+        break
+    print(s.strip())
+
+from io import BytesIO
+f = BytesIO()
+f.write('中文'.encode('utf-8'))
+
+print(f.getvalue())
+
+import pickle
+d = dict(name='Bob', age=20, score=88)
+ss = pickle.dumps(d)
+print(ss)
+
+
+# project_list
