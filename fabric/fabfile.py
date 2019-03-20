4c9c29130fe35bb0330a5d74f162e8f1c826978f
heyu <heyu@huayingjuhe.com>
2019-03-20 17:00:13 +0800

    update

--- fabric/fabfile.py
+++
@@ -1,9 +0,0 @@
-def hello():
-    print("""
-          测试 fabric is a deploy tool
-          you use a little code to do repeat thing
-          is very convenient [方便，便利，便于]
-          """)
-    print("hello world")
-
-hello()
