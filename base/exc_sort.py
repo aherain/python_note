461e4cd87d3fde64e731bfa667159d276d5ab868
heyu <heyu@huayingjuhe.com>
2019-03-29 14:32:37 +0800

    update

--- base/exc_sort.py
+++ base/exc_sort.py
@@ -1,24 +1,26 @@
 alist = [23,54,6,787,88,1,-1, 29]
 
+#将冒泡排序变成短冒泡排序，提高效率
 def bubbleSort(ls):
     exchanges = True
-    for i in range(len(ls)-1, 0, -1):
+    for i in range(len(ls)-1,0,-1):
         if exchanges:
             exchanges = False
-            for j in range(i):
-                if ls[j]>ls[j+1]:
-                    exchanges = True
-                    temp = ls[j]
-                    ls[j] = ls[j+1]
-                    ls[j+1] = temp
+        for j in range(i):
+            if ls[j]>ls[j+1]:
+                exchanges = True
+                temp = ls[j]
+                ls[j] = ls[j+1]
+                ls[j+1] = temp
 
 # bubbleSort(alist)#冒泡排序与短冒泡排序
 # print('冒泡排序与短冒泡排序的结果', alist)
 
+
 def selectSort(ls):
     for lt in range(len(ls)-1, 0, -1):
         positionOfMax = 0
-        #找到列表中最大的值
+        #找到列表中最大的值的下标，每次都需要将数组的待排序部分，从新扫描一遍
         for it in range(0, lt+1):
             if ls[it]>ls[positionOfMax]:
                 positionOfMax = it
@@ -30,13 +32,15 @@ def selectSort(ls):
 # print('选择排序的结果:', alist)
 
 
+
+
 #插入排序，顾名思义找到一个数的应有位置，然后插入进去
 def insertSort(ls):
     for i in range(1, len(ls)):
         pos, val = i, ls[i]
         while pos > 0 and ls[pos-1] > val:
             ls[pos] = ls[pos-1]
-            pos = pos - 1  # 从第二个值开始到最后一个值，拿一个数值， 然后在前面的有序部分找位置插入进去
+            pos = pos - 1  # 预留第一个位置，然后向前追溯，找到适合的位置放置当前值。
         ls[pos] = val
 
 # insertSort(alist)
@@ -56,7 +60,6 @@ def shellSort(ls):
 # shellSort(alist)
 # print('希尔排序的结果', alist)
 
-
 from collections import deque
 def merge_sort(lst):
     if len(lst) <= 1:
@@ -64,37 +67,36 @@ def merge_sort(lst):
     def merge(left, right):
         merged, left, right = deque(), deque(left), deque(right)
         while left and right:
-            merged.append(left.popleft() if left[0] <= right[0] else right.popleft())  # deque popleft is also O(1)
+            dd = left.popleft() if left[0] <= right[0] else right.popleft()
+            merged.append(dd)
+
         merged.extend(right if right else left)
-        print('合并后的数组', merged)
         return merged
     middle = int(len(lst) // 2)
-    left = merge_sort(lst[:middle])
-    right = merge_sort(lst[middle:])
+
+    left = merge_sort(lst[:middle])#左拆
+    right = merge_sort(lst[middle:])#右拆 然后合并在一起
     return merge(left, right)
 # merge_sort(alist)
-# print("归并排序的结果", merge_sort(alist))
-
-
 from collections import deque
-def merge_sort(lst):
-    if len(lst) <= 1:
-        return lst
+def merge_sort(ls):
+    if len(ls)<=1:
+        return ls
     def merge(left, right):
-        merged, left, right = deque, deque(left), deque(right)
+        merged, left, right = deque(), deque(left), deque(right)
         while left and right:
-            merged.append(left.popleft() if left[0] <= right[0] else right.popleft())
-        merged.extend( right if right else left)
+            dd = left.popleft() if left[0]<=right[0] else right.popleft()
+            merged.append(dd)
+
+        merged.extend(right if right else left)
         return merged
-    middle = int(len(lst) // 2)
-    left = merge_sort(lst[:middle])
-    right = merge_sort(lst[middle:])
-    print("left：", left , "\n", "right：", right)
+    middle = int(len(ls)//2)
+    left = merge_sort(ls[:middle])
+    right = merge_sort(ls[middle:])
     return merge(left, right)
-merge_sort(alist)
 
 
-#Talj is cheap, show the code
+#Talk is cheap, show the code
 def quicksort(nums):
     if len(nums) <= 1:
         return nums
@@ -106,58 +108,6 @@ def quicksort(nums):
         else:
             greater.append(x)
     return quicksort(less) + [base] + quicksort(greater)
-# nums = [6, 1,2,7,9,3,4,5,-1,8]
-# print('打印快速排序的结果：', quicksort(nums))
 
-
-# coding:utf-8
-# 找零钱问题算法实现：基本版性能分析
-
-# 统计递归次数
-recursion_num = 0
-# 4种硬币面值
-values = [1,5,10,25]
-# 缓存数组，为一个一维数组，用于缓存每次递归函数求得的值
-# cache[i]表示凑够i美分所需的最少硬币个数，cache的元素都被初始化为-1，表示个数未知
-cache = []
-# 初始化缓存数组
-def init(amount):
-    global cache
-    cache = [-1] * (amount + 1)
-
-# 凑够amount这么多钱数需要的最少硬币个数
-def minCoins(amount):
-    global recursion_num
-    global cache
-    # 需要的最少硬币个数
-    ret_min = amount
-    # 如果缓存数组中有对应的值，那么直接从中取，不再重复计算了
-    if cache[amount] != -1:
-        ret_min = cache[amount]
-    elif amount < 1:
-        ret_min = 0
-    # 如果要找的钱数恰好是某种硬币的面值，那么最少只需一个硬币
-    elif amount in values:
-        ret_min = 1
-    else:
-        # 遍历面值数组中面值小于等于amount的那些元素
-        for v in [x for x in values if x <= amount]:
-            # 用面值为v的硬币+其他硬币找零所需的最少硬币数
-            print(v, amount - v)
-            min_num = 1 + minCoins(amount - v)
-            # 判断min_num和ret_min的大小，更新ret_min
-            if min_num < ret_min:
-                ret_min = min_num
-    # 更新缓存数组
-    cache[amount] = ret_min
-    recursion_num += 1
-    return ret_min
-
-def main():
-    pass
-    init(63)
-    print('找零钱问题',minCoins(63))
-    # print(cache)
-    # print(recursion_num)
-#
-main()
+nums = [6, 1,2,7,9,3,4,5,-1,8]
+print('打印快速排序的结果：', quicksort(nums))
