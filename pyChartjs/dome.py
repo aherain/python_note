d2bfac4e13ce5a6e3bd667a2e58c39186c4eed7e
heyu <heyu@huayingjuhe.com>
2019-04-18 10:45:37 +0800

    pycharts

---
+++ pyChartjs/dome.py
@@ -0,0 +1,24 @@
+from snapshot_selenium import snapshot as driver
+
+from pyecharts import options as opts
+from pyecharts.charts import Bar
+from pyecharts.render import make_snapshot
+
+
+def bar_chart() -> Bar:
+    c = (
+        Bar()
+        .add_xaxis(["衬衫", "毛衣", "领带", "裤子", "风衣", "高跟鞋", "袜子"])
+        .add_yaxis("商家A", [114, 55, 27, 101, 125, 27, 105])
+        .add_yaxis("商家B", [57, 134, 137, 129, 145, 60, 49])
+        .reversal_axis()
+        .set_series_opts(label_opts=opts.LabelOpts(position="right"))
+        .set_global_opts(title_opts=opts.TitleOpts(title="Bar-测试渲染图片"))
+    )
+    return c
+
+# 需要安装 snapshot_selenium 也需要下载chromedriver 配置对应的环境变量
+make_snapshot(driver, bar_chart().render(), "bar.png")
+
+#数据交锋，是谁在说谎呢
+#预测未来，就是揣度上帝
